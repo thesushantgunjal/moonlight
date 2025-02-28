37c445ebeed02ff156abce19e939cf4cde950145
Sumit Harishchandra Mhaske <sumitmhaske435@gmail.com>
2025-02-28 13:26:56 +0530

    update

--- application/views/admin/footer.php
+++ application/views/admin/footer.php
@@ -1,41 +1,40 @@
  <!-- / Layout page -->
  </div>
 
-<!-- Overlay -->
-<div class="layout-overlay layout-menu-toggle"></div>
-</div>
-<!-- / Layout wrapper -->
+ <!-- Overlay -->
+ <div class="layout-overlay layout-menu-toggle"></div>
+ </div>
+ <!-- / Layout wrapper -->
 
-<div class="buy-now">
-<a
-  href="https://themeselection.com/item/sneat-dashboard-pro-bootstrap/"
-  target="_blank"
-  class="btn btn-danger btn-buy-now"
-  >Upgrade to Pro</a
->
-</div>
+ <div class="buy-now">
+     <a
+         href="https://themeselection.com/item/sneat-dashboard-pro-bootstrap/"
+         target="_blank"
+         class="btn btn-danger btn-buy-now">Upgrade to Pro</a>
+ </div>
 
-<!-- Core JS -->
-<!-- build:js assets/vendor/js/core.js -->
+ <!-- Core JS -->
+ <!-- build:js assets/vendor/js/core.js -->
 
-<script src="<?= base_url() ?>assets/admin/vendor/libs/jquery/jquery.js"></script>
-<script src="<?= base_url() ?>assets/admin/vendor/libs/popper/popper.js"></script>
-<script src="<?= base_url() ?>assets/admin/vendor/js/bootstrap.js"></script>
-<script src="<?= base_url() ?>assets/admin/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
-<script src="<?= base_url() ?>assets/admin/vendor/js/menu.js"></script>
+ <script src="<?= base_url() ?>assets/admin/vendor/libs/jquery/jquery.js"></script>
+ <script src="<?= base_url() ?>assets/admin/vendor/libs/popper/popper.js"></script>
+ <script src="<?= base_url() ?>assets/admin/vendor/js/bootstrap.js"></script>
+ <script src="<?= base_url() ?>assets/admin/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+ <script src="<?= base_url() ?>assets/admin/vendor/js/menu.js"></script>
 
-<!-- endbuild -->
+ <!-- endbuild -->
 
-<!-- Vendors JS -->
-<script src="<?= base_url() ?>assets/admin/vendor/libs/apex-charts/apexcharts.js"></script>
+ <!-- Vendors JS -->
+ <script src="<?= base_url() ?>assets/admin/vendor/libs/apex-charts/apexcharts.js"></script>
 
-<!-- Main JS -->
-<script src="<?= base_url() ?>assets/admin/js/main.js"></script>
+ <!-- Main JS -->
+ <script src="<?= base_url() ?>assets/admin/js/main.js"></script>
 
-<!-- Page JS -->
-<script src="<?= base_url() ?>assets/admin/js/dashboards-analytics.js"></script>
+ <!-- Page JS -->
+ <script src="<?= base_url() ?>assets/admin/js/dashboards-analytics.js"></script>
 
-<!-- Place this tag before closing body tag for github widget button. -->
-<script async defer src="https://buttons.github.io/buttons.js"></script>
-</body>
-</html>
+ <!-- Place this tag before closing body tag for github widget button. -->
+ <script async defer src="https://buttons.github.io/buttons.js"></script>
+ </body>
+
+ </html>
