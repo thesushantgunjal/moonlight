21842065166465e84ef65d80cffebe43233f3d0b
Sushant Gunjal <officialsushant15@gmail.com>
2025-03-02 15:54:00 +0530

    update

--- application/views/admin/navbar.php
+++ application/views/admin/navbar.php
@@ -116,38 +116,36 @@
               <span class="badge rounded-pill bg-danger ms-auto">5</span>
             </a>
           </li>
-
-          <!-- Layouts -->
+          <!-- Category -->
+          <li class="menu-item">
+            <a href="javascript:void(0);" class="menu-link">
+              <i class="menu-icon tf-icons bx bx-home-smile"></i>
+              <div class="text-truncate" data-i18n="Category">Category</div>
+            </a>
+          </li>
+          <!-- Rooms -->
+          <li class="menu-item">
+            <a href="javascript:void(0);" class="menu-link">
+              <i class="menu-icon tf-icons bx bx-home-smile"></i>
+              <div class="text-truncate" data-i18n="Rooms">Rooms</div>
+            </a>
+          </li>
+          <!-- Booking -->
           <li class="menu-item">
             <a href="javascript:void(0);" class="menu-link menu-toggle">
               <i class="menu-icon tf-icons bx bx-layout"></i>
-              <div class="text-truncate" data-i18n="Layouts">Layouts</div>
+              <div class="text-truncate" data-i18n="Booking">Booking</div>
             </a>
 
             <ul class="menu-sub">
               <li class="menu-item">
                 <a href="layouts-without-menu.html" class="menu-link">
-                  <div class="text-truncate" data-i18n="Without menu">Without menu</div>
+                  <div class="text-truncate" data-i18n="Booked Rooms">Booked Rooms</div>
                 </a>
               </li>
               <li class="menu-item">
-                <a href="layouts-without-navbar.html" class="menu-link">
-                  <div class="text-truncate" data-i18n="Without navbar">Without navbar</div>
-                </a>
-              </li>
-              <li class="menu-item">
-                <a href="layouts-fluid.html" class="menu-link">
-                  <div class="text-truncate" data-i18n="Fluid">Fluid</div>
-                </a>
-              </li>
-              <li class="menu-item">
-                <a href="layouts-container.html" class="menu-link">
-                  <div class="text-truncate" data-i18n="Container">Container</div>
-                </a>
-              </li>
-              <li class="menu-item">
-                <a href="layouts-blank.html" class="menu-link">
-                  <div class="text-truncate" data-i18n="Blank">Blank</div>
+                <a href="layouts-without-menu.html" class="menu-link">
+                  <div class="text-truncate" data-i18n="Check In">Check In</div>
                 </a>
               </li>
             </ul>
