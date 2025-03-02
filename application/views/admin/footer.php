d7f71a7555167b58a32994ed4f8682292ea8cc9f
Sushant Gunjal <officialsushant15@gmail.com>
2025-03-02 15:41:31 +0530

    admin setup

---
+++ application/views/admin/footer.php
@@ -0,0 +1,72 @@
+</div>
+<!-- / Content -->
+
+<!-- Footer -->
+<footer class="content-footer footer bg-footer-theme">
+    <div class="container-xxl">
+        <div class="footer-container d-flex align-items-center justify-content-between py-4 flex-md-row flex-column">
+            <div class="text-body">
+                ©
+                <script>
+                    document.write(new Date().getFullYear());
+                </script>
+                , made with ❤️ by
+                <a href="https://themeselection.com" target="_blank" class="footer-link">ThemeSelection</a>
+            </div>
+            <div class="d-none d-lg-inline-block">
+                <a href="https://themeselection.com/license/" class="footer-link me-4" target="_blank">License</a>
+                <a href="https://themeselection.com/" target="_blank" class="footer-link me-4">More Themes</a>
+
+                <a href="https://demos.themeselection.com/sneat-bootstrap-html-admin-template/documentation/"
+                    target="_blank" class="footer-link me-4">Documentation</a>
+
+                <a href="https://github.com/themeselection/sneat-html-admin-template-free/issues" target="_blank"
+                    class="footer-link">Support</a>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- / Footer -->
+
+<div class="content-backdrop fade"></div>
+</div>
+<!-- Content wrapper -->
+</div>
+<!-- / Layout page -->
+</div>
+
+<!-- Overlay -->
+<div class="layout-overlay layout-menu-toggle"></div>
+</div>
+<!-- / Layout wrapper -->
+
+<div class="buy-now">
+    <a href="https://themeselection.com/item/sneat-dashboard-pro-bootstrap/" target="_blank"
+        class="btn btn-danger btn-buy-now">Upgrade to Pro</a>
+</div>
+
+<!-- Core JS -->
+<!-- build:js assets/vendor/js/core.js -->
+
+<script src="<?= base_url() ?>/assets/admin/vendor/libs/jquery/jquery.js"></script>
+<script src="<?= base_url() ?>/assets/admin/vendor/libs/popper/popper.js"></script>
+<script src="<?= base_url() ?>/assets/admin/vendor/js/bootstrap.js"></script>
+<script src="<?= base_url() ?>/assets/admin/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+<script src="<?= base_url() ?>/assets/admin/vendor/js/menu.js"></script>
+
+<!-- endbuild -->
+
+<!-- Vendors JS -->
+<script src="<?= base_url() ?>/assets/admin/vendor/libs/apex-charts/apexcharts.js"></script>
+
+<!-- Main JS -->
+<script src="<?= base_url() ?>/assets/admin/js/main.js"></script>
+
+<!-- Page JS -->
+<script src="<?= base_url() ?>/assets/admin/js/dashboards-analytics.js"></script>
+
+<!-- Place this tag before closing body tag for github widget button. -->
+<script async defer src="https://buttons.github.io/buttons.js"></script>
+</body>
+
+</html>
